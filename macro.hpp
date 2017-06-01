7a004d8c39ed71887c4f48cc70d99d92d44fd573
Bernardo Gomes <bernardogomes@Bernardos-MBP.lan>
2017-06-01 11:14:24 +0100

    4658 iterations to get result

--- macro.hpp
+++ macro.hpp
@@ -1,7 +1,7 @@
 /*Varaibles definitions*/
-#define GA_TARGET "Hello World!" 
-#define GA_POPSIZE 20 //population size
-#define TOURNAMENTSIZE 5 //tournament population size
+#define GA_TARGET "A D.Aurilia nao tem nenhuma outra fonte de rendimento para alem dos juros das suas poupancas."
+#define GA_POPSIZE 500 //population size
+#define TOURNAMENTSIZE 3 //tournament population size
 #define UNIFORMRATE 0.5 //decision of crossover
 #define MUTATIONRATE 0.015
 
