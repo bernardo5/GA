333f15f3ca94d6d10e4c7d2978761b740f3e06a2
bernardo5 <bernardo.n.gomes@tecnico.ulisboa.pt>
2017-06-08 14:50:29 +0100

    write file with optimum values and time elapsed of program run

--- numerical/ga.cpp
+++ numerical/ga.cpp
@@ -5,28 +5,31 @@
 #include <string>
 #include <ctime>
 #include <chrono>
+#include <fstream>
 //using expressions
 using namespace std;
 
+void writeFile(int*finalSequence, string final_time){
+	std::ofstream outputFile;
+	outputFile.open("output.txt", std::ios_base::app);
+	//checkVerifications
+	for(int i=0; i<NUMBERVARIABLES; i++){
+		outputFile<<to_string(finalSequence[i])+"\t";	
+	}	
+	outputFile<<final_time+"\n";
+	return;
+}	
+
 int main(){
 	int i=1;
-	int old_fitness=0;
-	float threshold=0.001;
 	//initialize rand parameter
 	srand(time(0));
 	//start counting clock
 	auto start_time = std::chrono::high_resolution_clock::now();
 	//start GA
 	population *pop=new population();
-	//pop->printRates();
 	pop->calcPopFitness();
-	
-	//pop->printPopulation();
-	
-	
 	pop->popSort();
-	
-	//pop->printPopulation();
 	string s=pop->getElement(0).getString();
 	while(pop->getElement(0).getFitness()!=0){
 		pop->evolvePop();
@@ -41,5 +44,6 @@ int main(){
 	auto time = end_time - start_time;
 	std::cout << "It took " <<
     std::chrono::duration_cast<std::chrono::milliseconds>(time).count() << " milliseconds to run.\n";
+    writeFile(pop->getElement(0).getValues(), to_string(std::chrono::duration_cast<std::chrono::milliseconds>(time).count()));
 	return 0;
 }
