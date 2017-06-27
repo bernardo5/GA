c70a8462c87c02ed1b813231475be2376628ed2b
bernardo5 <bernardo.n.gomes@tecnico.ulisboa.pt>
2017-06-27 11:41:15 +0100

    tests run

--- numerical/macro.hpp
+++ numerical/macro.hpp
@@ -1,6 +1,6 @@
 /*Varaibles definitions*/
-#define NUMBERVARIABLES 8
-#define GA_POPSIZE 60 //population size
+#define NUMBERVARIABLES 4
+#define GA_POPSIZE 250 //population size
 #define TOURNAMENTSIZE 3 //tournament population size
 #define UNIFORMRATE 0.5 //decision of crossover
 #define MUTATIONRATE 0.015
