0523f2a72496157fcd807e777bd9c94fa913c508
bernardo5 <bernardo.n.gomes@tecnico.ulisboa.pt>
2017-06-27 16:01:17 +0100

    interprocess communication

--- helloWorldMPI/test.cpp
+++ helloWorldMPI/test.cpp
@@ -23,7 +23,46 @@ int main(){
 	int name_len;
 	MPI_Get_processor_name(processor_name, &name_len);
 	
-	cout<<"Hello world from processor "+string(processor_name)+", rank "+to_string(world_rank)+" out of "+to_string(world_size)+" number of processes.\n";
+	//0 will be master flag
+	//1 will be rank 1 slave flag
+	//2 will be rank 2 slave flag
+	int number, number1;
+	if(world_rank==0){
+		cout<<"Master talking here...\n";
+		cout<<"Hello world from processor "+string(processor_name)+", rank "+to_string(world_rank)+" out of "+to_string(world_size)+" number of processes.\n";
+		cout<<"\n\n";
+		number = 0;
+		MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+	}else{
+		if(world_rank==1){
+			MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+			printf("Slave 1 received number %d from master\n",
+				   number);
+			
+			cout<<"Slave talking here...\n";
+			cout<<"Hello world from processor "+string(processor_name)+", rank "+to_string(world_rank)+" out of "+to_string(world_size)+" number of processes.\n";
+			cout<<"\n\n";
+			number1=1;
+			MPI_Send(&number1, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+			
+		}else{
+			//MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
+            // MPI_STATUS_IGNORE);
+            
+            MPI_Recv(&number1, 1, MPI_INT, 1, 0, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+             
+			printf("Slave 2 received number %d from slave 1\n",
+				   number1);
+			
+			
+			cout<<"Slave talking here...\n";
+			cout<<"Hello world from processor "+string(processor_name)+", rank "+to_string(world_rank)+" out of "+to_string(world_size)+" number of processes.\n";
+			cout<<"\n\n";
+		}
+	}
+	
 	
 	MPI_Finalize();
 	
